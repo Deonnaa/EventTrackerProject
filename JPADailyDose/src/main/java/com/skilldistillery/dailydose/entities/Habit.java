a44de3b729a528c28a47d355fad409b10bdfdd7b
Deonnaa <aponte.deonna@gmail.com>
2024-01-05 18:56:27 -0500

    Configure JPA, HabitTest

---
+++ JPADailyDose/src/main/java/com/skilldistillery/dailydose/entities/Habit.java
@@ -0,0 +1,107 @@
+package com.skilldistillery.dailydose.entities;
+
+import java.sql.Date;
+import java.util.Objects;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "habit")
+public class Habit {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	private String description;
+
+	@Column(name = "start_date", nullable = false)
+	private Date startDate;
+
+	private String frequency;
+
+	private Boolean status;
+
+	public Habit() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getFrequency() {
+		return frequency;
+	}
+
+	public void setFrequency(String frequency) {
+		this.frequency = frequency;
+	}
+
+	public Boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(Boolean status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Habit [id=" + id + ", name=" + name + ", description=" + description + ", startDate=" + startDate
+				+ ", frequency=" + frequency + ", status=" + status + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Habit other = (Habit) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
